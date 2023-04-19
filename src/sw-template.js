08b66706f45f42330e633c7ae1a660f8ebb4809a
bfernandez <bfernandez@seidor.es>
2023-04-19 17:25:52 +0200

    Service worker con workbox añadido

---
+++ src/sw-template.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+workbox.loadModule('workbox-background-sync');
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync
+
+const cacheFirst = [
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
+];
+
+registerRoute(
+    ({ request, url }) => {
+        if (cacheFirst.includes(url.href)) return true;
+        return false;
+    },
+    new CacheFirst()
+);
+
+// registerRoute(
+//     new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
+//     new CacheFirst()
+// )
+
+// registerRoute(
+//     new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
+//     new CacheFirst()
+// )
+
+const cacheNetworkFirst = [
+    '/api/auth/renew',
+    '/api/events'
+];
+
+registerRoute(
+    ({ request, url }) => {
+        if (cacheNetworkFirst.includes(url.pathname)) return true;
+        return false;
+    },
+    new NetworkFirst()
+);
+
+// registerRoute(
+//     new RegExp('http://localhost:4000/api/auth/renew'),
+//     new NetworkFirst()
+// )
+
+// registerRoute(
+//     new RegExp('http://localhost:4000/api/events'),
+//     new NetworkFirst()
+// )
+
+// Post offline
+const bgSyncPluginPost = new BackgroundSyncPlugin('post-offline', {
+    maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+// Update offline
+const bgSyncPluginUpdate = new BackgroundSyncPlugin('update-offline', {
+    maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+// Delete offline
+const bgSyncPluginDelete = new BackgroundSyncPlugin('delete-offline', {
+    maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+    new RegExp('http://localhost:4000/api/events'),
+    new NetworkOnly({
+        plugins: [bgSyncPluginPost]
+    }),
+    'POST'
+)
+
+registerRoute(
+    new RegExp('http://localhost:4000/api/events'),
+    new NetworkOnly({
+        plugins: [bgSyncPluginUpdate]
+    }),
+    'PUT'
+)
+
+registerRoute(
+    new RegExp('http://localhost:4000/api/events'),
+    new NetworkOnly({
+        plugins: [bgSyncPluginDelete]
+    }),
+    'DELETE'
+)
